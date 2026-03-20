d32c1aef5d93abbccb06ca1aba4cdc07fd0df5ce
Akhil <akhiljohnson2000@gmail.com>
2026-03-20 20:44:34 +0530

    Page refresh issue fixed

--- portfolio/src/router/index.ts
+++ portfolio/src/router/index.ts
@@ -1,7 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'Home', component: () => import('@/views/HomeView.vue') },
     { path: '/learn', name: 'Learn', component: () => import('@/views/LearnView.vue') },
